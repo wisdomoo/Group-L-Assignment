dd26227f4f672b0f7c468907eb96bde5ea9da772
wisdom <okoroaforwisdomo@gmail.com>
2023-10-17 11:59:38 +0100

    code modified

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Get references to the input field and the provider logo element
 const phoneInput = document.querySelector('.phone');
 const providerLogo = document.querySelector('.provider-logo');
+const errormsg = document.querySelector('.error')
 
 // Function to detect and set the provider logo
 function detectProvider(phoneNumber) {
-  const mtnPattern = /^(0703|0803|081|0903|0806|0706|\+234703|\+234803)/;
+  const mtnPattern = /^(0703|0803|0813|0903|0806|0706|\+234703|\+234803|\+234813|\+234903|\+234806|\+234706)/;
   const gloPattern = /^(0705|0805|0807|0815|0905|\+234705|\+234805|\+234807|\+234815|\+234905)/;
   const airtelPattern = /^(0701|0802|0808|0812|0901|\+234701|\+234802|\+234808|\+234812|\+234901)/;
-  const nineMobilePattern = /^(0809|0817|0818|0909)/;
+  const nineMobilePattern = /^(0809|0817|0818|0909|\+234809|\+234817|\+234909)/;
 
   providerLogo.classList.remove('mtn', 'glo', 'airtel', 'nineMobile');
+  errormsg.classList.remove('errormsg')
+  
 
   if (mtnPattern.test(phoneNumber)) {
     providerLogo.classList.add('mtn');
@@ -20,10 +23,19 @@ function detectProvider(phoneNumber) {
   } else if (nineMobilePattern.test(phoneNumber)) {
     providerLogo.classList.add('nineMobile');
   }
+  else{
+    errormsg.classList.add('errormsg')
+  }
+  
 }
 
 // Event listener to detect changes in the input field
 phoneInput.addEventListener('input', function() {
   const phoneNumber = this.value;
   detectProvider(phoneNumber);
+  if (phoneInput.value === ''){
+    errormsg.classList.remove('errormsg')
+
+  }
+  
 });
